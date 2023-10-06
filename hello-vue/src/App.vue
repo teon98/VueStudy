b95ee3797e30deb0256b0d0914e8bca70d5edb18
Tae Young <49816869+teon98@users.noreply.github.com>
2023-10-06 18:04:10 +0900

    Data Binding

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -4,6 +4,33 @@
   <!-- html 담당-->
   <div>
     <Header />
+    <h3>{{ title }}</h3>
+    <input v-model="title" />
+    <input
+      type="checkbox"
+      id="프롬스타"
+      value="프롬스타"
+      v-model="checkedWebtoons"
+    />
+    <label for="프롬스타">프롬스타</label>
+    <input
+      type="checkbox"
+      id="햄스터와그녀"
+      value="햄스터와 그녀"
+      v-model="checkedWebtoons"
+    />
+    <label for="햄스터와그녀">햄스터와 그녀</label>
+    <input
+      type="checkbox"
+      id="위대한로맨스"
+      value="위대한 로맨스"
+      v-model="checkedWebtoons"
+    />
+    <label for="위대한로맨스">위대한 로맨스</label>
+    <input type="checkbox" id="아귀" value="아귀" v-model="checkedWebtoons" />
+    <label for="아귀">아귀</label>
+    <br />
+    <span>찜한 웹툰: {{ checkedWebtoons }}</span>
     <div class="wrap">
       <Menu />
       <Content />
@@ -24,6 +51,12 @@ export default {
     Header,
     Menu,
     Content,
+  },
+  data() {
+    return {
+      title: '',
+      checkedWebtoons: [],
+    };
   },
 };
 </script>
